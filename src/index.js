9c89d32f97e95b4e03760320d19057ba0e68c89c
Samuel Batista <samuel.faeng@gmail.com>
2020-01-13 16:07:06 -0300

    Primeira versao do codigo no template do codenation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 'use strict'
 
-const fibonacci = () => null
+const fibonacci = () => {
+    var indices = [];
+    let F = 0;
+    let i = 1;
+    while (F < 350) {
+        F = Math.round(((((1+Math.sqrt(5))/2)**i) - (((1-Math.sqrt(5))/2)**i))/(Math.sqrt(5)));
+        indices.push(F)
+        i++;
+    }
+    return indices
+}
 
-const isFibonnaci = (num) => null
+const isFibonnaci = (num) => {
+    let Lista = fibonacci(); 
+    var estaNaLista = [];
+    if (Lista.indexOf(num) != -1) {
+        estaNaLista = true;
+    } else {
+        estaNaLista = false;
+    }
+    return estaNaLista;
+}
+console.log(isFibonnaci(5));
 
 module.exports = {
     fibonacci,
